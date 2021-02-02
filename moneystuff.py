e17d2a39b250c1ca78355a9c8efb1cc3856fac8e
Cosmo <78331550+randomcodingstuff@users.noreply.github.com>
2021-02-02 09:38:45 +0800

    Add files via upload

---
+++ moneystuff.py
@@ -0,0 +1,10 @@
+mscore=input("What is your math score? ")
+escore=input("What is your English score? ")
+mscore=int(mscore)
+escore=int(escore)
+if (mscore>=90 and escore>=90):
+    print("You get money! ")
+elif(mscore==100 or escore==100):
+    print("You get money! ")
+else :
+    print("You don't get money! ")
