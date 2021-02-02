4bd5e86c2ab622248f2c66b9e75d74b51f58a810
Cosmo <78331550+randomcodingstuff@users.noreply.github.com>
2021-02-02 10:23:03 +0800

    Add files via upload

--- moneystuff.py
+++ moneystuff.py
@@ -1,10 +1,8 @@
-mscore=input("What is your math score? ")
-escore=input("What is your English score? ")
-mscore=int(mscore)
-escore=int(escore)
-if (mscore>=90 and escore>=90):
+mscore=input(("What is your math score? "))#asking for score
+escore=int(input("What is your English score? "))#asking for score
+if not (mscore>=0 and mscore<=)and(escore2>=0 and escore<=100):#is score valid
+    print("Re-enter your grade! ")
+elif (mscore>=90 and escore>=90)or(mscore==100 or escore==100):#get money not
     print("You get money! ")
-elif(mscore==100 or escore==100):
-    print("You get money! ")
-else :
+else :#no get money
     print("You don't get money! ")
